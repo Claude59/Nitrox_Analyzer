4d7e38a3bb9fa53d9d3a4cbbb5493770fdf0698a
Charles Fourneau <charles.fourneau@gmail.com>
2020-01-05 09:01:50 +0100

    Update RollingAverage.h/.cpp

--- lib/RollingAverage/RollingAverage.cpp
+++ lib/RollingAverage/RollingAverage.cpp
@@ -1,33 +1,35 @@
 #include <RollingAverage.h>
 
 RollingAverage::RollingAverage(uint8_t s) {
-    size = s;
-    readings = (uint16_t*) malloc(size * sizeof(uint16_t));
-    if (readings == NULL) size = 0;
+	size = s;
+	values = (int16_t*) malloc(size * sizeof(int16_t));
+	if (values == NULL) size = 0;
 }
 
 void RollingAverage::init() {
-    sum = 0;
-    for (uint8_t i; i < size; i++) {
-        readings[i] = 0;
-    }
-    index = 0;
-    n_readings = 0;
+	sum = 0;
+	for (uint8_t i; i < size; i++) {
+		values[i] = 0;
+	}
+	index = 0;
+	n_values = 0;
 }
 
-void addReading(uint16_t value) {
-    if (n_readings < size) {
-        n_readings++;
-        sum += value;
-    }
-    else {
-        sum = sum - readings[index] + value;
-    }
+void RollingAverage::addValue(int16_t value) {
+	if (n_values < size) {
+		n_values++;
+		sum += value;
+	}
+	else {
+		sum = sum - values[index] + value;
+	}
 
-    readings[index] = value;
-    index = (index == size - 1) ? 0 : index + 1;
+	values[index] = value;
+	index = (index == size - 1) ? 0 : index + 1;
 }
 
-uint16_t getAverage() {
-    return (sum + n_readings / 2) / n_readings;
+int16_t RollingAverage::getAverage() {
+//	return (int16_t)((sum + n_values / 2) / n_values);	// using (+ n_values / 2) reduce truncation errors by rounding
+														// only useful if n_values is large and values are relatively small
+	return (int16_t)(sum / n_values);	
 }
--- lib/RollingAverage/RollingAverage.h
+++ lib/RollingAverage/RollingAverage.h
@@ -1,23 +1,24 @@
 #ifndef _ROLLING_AVERAGE_H_
 #define _ROLLING_AVERAGE_H_
 
+#include <Arduino.h>
+
 class RollingAverage {
 public:
     RollingAverage();
     RollingAverage(uint8_t size);
     void init();
-    void init(uint16_t value);
 
 public:
-    void addReading(uint16_t value);
-    uint16_t getAverage();
+    void addValue(int16_t value);
+    int16_t getAverage();
 
 private:
     uint8_t size;
     uint8_t index;
-    uint8_t n_readings;
-    uint16_t* readings;
-    uint16_t sum;
+    uint8_t n_values;
+    int16_t* values;
+    int32_t sum;
 };
 
 #endif // _ROLLING_AVERAGE_H_
